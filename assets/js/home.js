0ed03a4963c746c0141851b13ad7b3928f00c53b
SergAlKoba <Serg.Al.Koba@gmail.com>
2020-11-10 15:09:17 +0200

    home 50%

---
+++ assets/js/home.js
@@ -0,0 +1,144 @@
+$(document).ready(function(){
+
+    $('.banner-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+        prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                arrows: false,
+                adaptiveHeight: true
+              }
+            }
+        ]
+    });
+
+    $('.companies-slider').slick({
+        dots: false,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+        prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                dots: true,
+                arrows: false,
+                slidesToShow: 5,
+                slidesToScroll: 4,
+              }
+            }
+        ]
+    });
+
+    $('.best-deals-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+        prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+        responsive: [
+            {
+              breakpoint: 1280,
+              settings: {
+                dots: false,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+                prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                dots: true,
+                arrows: false,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                dots: true,
+                arrows: false,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+        ]
+    });
+
+    $('.companies-item').matchHeight({
+        byRow: false
+    }); 
+
+    $('.why-leader-item .icon').matchHeight({
+        byRow: false
+    }); 
+
+    $('.best-deals-item .name').matchHeight({
+        byRow: false
+    }); 
+
+    if($(window).width() >= 1024){
+
+        $('.banner-item .content').matchHeight({
+            byRow: false
+        }); 
+
+    }
+
+    $('.insurance-item .img').height( $('.insurance-item .img').width() / 1.83 );
+
+    if($(window).width() <= 767){
+        $('.insurance-items').height( ( $('.insurance-item').height() + 10.5 ) * 4 );
+    }else{
+        $('.insurance-items').css({height: 'auto'})
+    }
+
+    $('.insurance-cost .show-more').click(function(){
+        var autoHeight = $('.insurance-hide').height();
+		$('.insurance-items').toggleClass('hide');	
+		if ($('.insurance-items').hasClass('hide')) {
+            $('.show-more').html('Еще предложения');
+            $('.insurance-items').animate({height: ( $('.insurance-item').height() + 10.5 ) * 4 + 'px'}, 1000)
+		} else {
+            $('.show-more').html('Скрыть');
+            //$('.hide').animate({height: autoHeight}, 1000)
+            $('.insurance-items').animate({height: autoHeight}, 1000);
+		}		
+		return false;
+    });	
+
+    if($(window).width() >= 768){
+        $('.best-deals-item .img').height( $('.insurance-item .img').width() / 1.12 );
+    }else{
+        $('.best-deals-item .img').height( $('.insurance-item .img').width() / 1.64 );
+    }
+
+});
+
+$(window).resize(function(){
+
+    $('.insurance-item .img').height( $('.insurance-item .img').width() / 1.83 );   
+    
+    if($(window).width() <= 767){
+        $('.insurance-items').height( ( $('.insurance-item').height() + 10.5 ) * 4 );
+    }else{
+        $('.insurance-items').css({height: 'auto'})
+    }
+
+    if($(window).width() >= 768){
+        $('.best-deals-item .img').height( $('.insurance-item .img').width() / 1.12 );
+    }else{
+        $('.best-deals-item .img').height( $('.insurance-item .img').width() / 1.64 );
+    }
+
+});
