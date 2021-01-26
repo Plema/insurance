4a090e2ae2e4a7785d43a748b30309b626a807eb
SergAlKoba <Serg.Al.Koba@gmail.com>
2021-01-26 08:44:37 +0200

    page about

---
+++ assets/js/about.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+   
+    $('.companies-slider').slick({
+        dots: false,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+        prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                dots: true,
+                arrows: false,
+                slidesToShow: 5,
+                slidesToScroll: 4,
+              }
+            }
+        ]
+    });
+
+    $('.companies-item').matchHeight({
+        byRow: false
+    }); 
+
+    $('.beneficial-slider').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        responsive: [
+            {
+              breakpoint: 1280,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                adaptiveHeight: true,
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                adaptiveHeight: true,
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  adaptiveHeight: true,
+                }
+            }
+        ]
+    });
+
+    $('.beneficial-item .text h4').matchHeight({
+      byRow: false
+    }); 
+    
+
+    $('.beneficial-item .img').height( $('.beneficial-item .img').width() / 1.07 );
+    
+});
+
+$(window).resize(function(){
+
+    $('.beneficial-item .img').height( $('.beneficial-item .img').width() / 1.07 );
+
+});
