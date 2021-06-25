43bf1e36570e22aa5d58db5bb7dfa51b43ac3041
Daniil-Borisov <daniil.borisov5@gmail.com>
2021-06-25 09:21:51 +0300

    25.06.21

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,26 +1,29 @@
 $(document).ready(function(){
    
-    $('.companies-slider').slick({
-        dots: false,
-        slidesToShow: 7,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay: true,
-        playSpeed: 4000,
-        nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
-        prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                dots: true,
-                arrows: false,
-                slidesToShow: 5,
-                slidesToScroll: 4,
-              }
-            }
-        ]
-    });
+  $('.companies-slider').slick({
+    dots: false,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 5000,
+    cssEase: 'linear',
+    arrows: false,
+    pauseOnHover: false,
+    // nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+    // prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            dots: true,
+            arrows: false,
+            slidesToShow: 5,
+            slidesToScroll: 4,
+          }
+        }
+    ]
+});
     $(window).blur(function() { 
       $('.companies-slider').slick('slickPause')
     }); //Покинули вкладку
