8809b4f52cb69dd1d36b63761301627a0dc13cf1
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-09-17 23:50:16 +0300

    js

--- assets/js/home.js
+++ assets/js/home.js
@@ -46,6 +46,7 @@ $(document).ready(function () {
     dots: false,
     slidesToShow: 7,
     slidesToScroll: 1,
+    infinite: true,
     autoplay: true,
     autoplaySpeed: 0,
     speed: 7000,
@@ -53,7 +54,11 @@ $(document).ready(function () {
     cssEase: 'linear',
     arrows: false,
     pauseOnHover: false,
+    pauseOnFocus: false,
+    draggable: false,
+    swipe: false,
     initialSlide: 5,
+    touchMove: false,
     // nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
     // prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
     responsive: [
@@ -192,30 +197,30 @@ $(document).ready(function () {
 
   $('.insurance-item .img').height($('.insurance-item .img').width() / 1.83)
 
-  if ($(window).width() <= 767) {
-    $('.insurance-items').height(($('.insurance-item').height() + 10.5) * 4)
-  } else {
-    $('.insurance-items').css({ height: 'auto' })
-  }
+  // if ($(window).width() <= 767) {
+  //   $('.insurance-items').height(($('.insurance-item').height() + 10.5) * 4)
+  // } else {
+  //   $('.insurance-items').css({ height: 'auto' })
+  // }
   if ($(window).width() < 1024) {
     $('.insurance-item .img').click(function () {
       $('ul').toggleClass('text-hover-show')
     })
   }
 
-  $('.insurance-cost .show-more').click(function () {
-    var autoHeight = $('.insurance-hide').height()
-    $('.insurance-items').toggleClass('hide')
-    if ($('.insurance-items').hasClass('hide')) {
-      $('.show-more').html('Еще предложения')
-      $('.insurance-items').animate({ height: ($('.insurance-item').height() + 10.5) * 4 + 'px' }, 1000)
-    } else {
-      $('.show-more').html('Скрыть')
-      //$('.hide').animate({height: autoHeight}, 1000)
-      $('.insurance-items').animate({ height: autoHeight }, 1000)
-    }
-    return false
-  })
+  // $('.insurance-cost .show-more').click(function () {
+  //   var autoHeight = $('.insurance-hide').height()
+  //   $('.insurance-items').toggleClass('hide')
+  //   if ($('.insurance-items').hasClass('hide')) {
+  //     $('.show-more').html('Еще предложения')
+  //     $('.insurance-items').animate({ height: ($('.insurance-item').height() + 10.5) * 4 + 'px' }, 1000)
+  //   } else {
+  //     $('.show-more').html('Скрыть')
+  //     //$('.hide').animate({height: autoHeight}, 1000)
+  //     $('.insurance-items').animate({ height: autoHeight }, 1000)
+  //   }
+  //   return false
+  // })
 
   if ($(window).width() >= 768) {
     $('.best-deals-item .img').height($('.insurance-item .img').width() / 1.12)
--- assets/js/internal-page.js
+++ assets/js/internal-page.js
@@ -3,10 +3,8 @@ $(document).ready(function () {
     dots: false,
     slidesToShow: 4,
     slidesToScroll: 1,
-    nextArrow:
-      '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
-    prevArrow:
-      '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+    nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+    prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
     responsive: [
       {
         breakpoint: 1280,
@@ -14,10 +12,8 @@ $(document).ready(function () {
           dots: false,
           slidesToShow: 3,
           slidesToScroll: 1,
-          nextArrow:
-            '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
-          prevArrow:
-            '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
+          nextArrow: '<button class="slick-arrow next"><i class="icon-arrow-next"></i></button>',
+          prevArrow: '<button class="slick-arrow prev"><i class="icon-arrow-prev"></i></button>',
         },
       },
       {
@@ -72,11 +68,11 @@ $(document).ready(function () {
 
   $('.insurance-item .img').height($('.insurance-item .img').width() / 1.83)
 
-  if ($(window).width() <= 767) {
-    $('.insurance-items').height(($('.insurance-item').height() + 10.5) * 4)
-  } else {
-    $('.insurance-items').css({ height: 'auto' })
-  }
+  // if ($(window).width() <= 767) {
+  //   $('.insurance-items').height(($('.insurance-item').height() + 10.5) * 4)
+  // } else {
+  //   $('.insurance-items').css({ height: 'auto' })
+  // }
 
   if ($(window).width() < 1024) {
     $('.insurance-item .img').click(function () {
@@ -84,22 +80,22 @@ $(document).ready(function () {
     })
   }
 
-  $('.insurance-cost .show-more').click(function () {
-    var autoHeight = $('.insurance-hide').height()
-    $('.insurance-items').toggleClass('hide')
-    if ($('.insurance-items').hasClass('hide')) {
-      $('.show-more').html('Еще предложения')
-      $('.insurance-items').animate(
-        { height: ($('.insurance-item').height() + 10.5) * 4 + 'px' },
-        1000
-      )
-    } else {
-      $('.show-more').html('Скрыть')
-      //$('.hide').animate({height: autoHeight}, 1000)
-      $('.insurance-items').animate({ height: autoHeight }, 1000)
-    }
-    return false
-  })
+  // $('.insurance-cost .show-more').click(function () {
+  //   var autoHeight = $('.insurance-hide').height()
+  //   $('.insurance-items').toggleClass('hide')
+  //   if ($('.insurance-items').hasClass('hide')) {
+  //     $('.show-more').html('Еще предложения')
+  //     $('.insurance-items').animate(
+  //       { height: ($('.insurance-item').height() + 10.5) * 4 + 'px' },
+  //       1000
+  //     )
+  //   } else {
+  //     $('.show-more').html('Скрыть')
+  //     //$('.hide').animate({height: autoHeight}, 1000)
+  //     $('.insurance-items').animate({ height: autoHeight }, 1000)
+  //   }
+  //   return false
+  // })
   if ($(window).width() >= 768) {
     $('.best-deals-item .img').height($('.insurance-item .img').width() / 1.12)
   } else {
