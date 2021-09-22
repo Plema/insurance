1468b91083a255e070a7b6604397296bc3f0571b
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-09-22 14:53:39 +0300

    fix

--- assets/js/shares.js
+++ assets/js/shares.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
   $('.shares-item .img').height($('.shares-item .img').width() / 1.85)
 
+  // if ($(window).width() < 1024) {
+  //   $('.shares-item .img').click(function () {
+  //     $(this).next().toggleClass('text-hover-show')
+  //   })
+  // }
   if ($(window).width() < 1024) {
     $('.shares-item .img').click(function () {
-      $('.text-hover').toggleClass('text-hover-show')
+      if ($(this).next().hasClass('text-hover-show')) {
+        $('.shares-item .text').removeClass('text-hover-show')
+      } else {
+        $('.shares-item .text').removeClass('text-hover-show')
+        $(this).next().addClass('text-hover-show')
+      }
     })
   }
 
